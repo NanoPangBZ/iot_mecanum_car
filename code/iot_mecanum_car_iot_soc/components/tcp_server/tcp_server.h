5abe063e28191a5df7d1f74e623a66c711afb66c
CodingBugStd <3096470072@qq.com>
2023-04-08 00:50:22 +0800

    refactor: 🎨tcp_server class重构-未完成

--- code/iot_mecanum_car_iot_soc/components/tcp_server/tcp_server.h
+++ code/iot_mecanum_car_iot_soc/components/tcp_server/tcp_server.h
@@ -5,14 +5,26 @@
 #include "freertos/task.h"
 #include "lwip/sockets.h"
 
+#define TCP_SERVER_ERROR    -1
+#define TCP_SERVER_LISTEN   0
+#define TCP_SERVER_RUNNING  1
+#define TCP_SERVER_STOP     2
+
+typedef int (*tcp_server_business_t)( int socket , void* user);
+
 class tcp_server{
 public:
-    tcp_server( uint16_t port );
+    tcp_server( tcp_server_business_t business );
     ~tcp_server();
+
+    void start( uint16_t listen_port , uint16_t task_stack_size );
+    void stop();
+    int  getState();
 
 private:
     TaskHandle_t _taskHandle;
     static void tcp_server_task( void* param );
+    tcp_server_business_t _business;
 
     char addr_str[128];
     int ip_protocol = 0;            //tcp协议
@@ -21,7 +33,10 @@ private:
     int keepInterval;
     int keepCount;
     int listen_sock;
+    int sock;
     struct sockaddr_storage dest_addr;
+
+    int _state;
 };
 
 #endif  //_TCP_SERVER_H_
