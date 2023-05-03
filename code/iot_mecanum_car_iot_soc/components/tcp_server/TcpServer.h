9e50642c71c63acb95df776c6c4ce91ec12ba45a
庞 <3096470072@qq.com>
2023-05-03 16:27:07 +0800

    fix: 🐛补充tcp服务端头文件

--- code/iot_mecanum_car_iot_soc/components/tcp_server/TcpServer.h
+++ code/iot_mecanum_car_iot_soc/components/tcp_server/TcpServer.h
@@ -3,15 +3,29 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
 #include <stdint.h>
+#include <list>
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
+class TcpServer;
+
 typedef void (*TcpApp)( int sock );
+
+typedef struct{
+    TcpApp _app;
+    int sock;
+    int keepAlive;
+    int keepIdle;
+    int keepInterval;
+    int keepCount;
+    TaskHandle_t taskHandle;
+    TcpServer*  server;
+}TcpServerAppCtx;
 
 class TcpServer{
 public:
@@ -24,10 +38,13 @@ private:
     static void _serverAppBridge(void* param);
     
     TaskHandle_t _listenTask;
+    int listen_sock;
     uint16_t _appStack;
     TcpApp _app;
     uint16_t _listenPort;
     int _connectCount;   //连接数量
+    std::list<TcpServerAppCtx*> tcpServerAppList;
+    SemaphoreHandle_t   appLock;
 };
 
 #endif  //_TCP_SERVER_H_
