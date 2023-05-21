cb74ace8107863c7875db14b98e7c3b097fa20f2
庞 <3096470072@qq.com>
2023-05-21 10:19:27 +0800

    feat: ✨ demo esp32c3部分wrp

--- code/iot_mecanum_car_iot_soc/main/main.cpp
+++ code/iot_mecanum_car_iot_soc/main/main.cpp
@@ -10,13 +10,18 @@
 #include "esp_log.h"
 #define TAG "main"
 
+static TcpServer tcpServer;
 static void uart_send_scp_pack( scp_pack_t* pack );
+static int tcp_send( uint8_t* data , uint16_t len );
 static SemaphoreHandle_t uart_tx_lock = NULL;
+static SemaphoreHandle_t tcp_tx_lock = NULL;
+static int _s_sock = -1;
 
 static void tcpApp( int sock )
 {
     uint8_t buf[512];
     int len = 0;
+    _s_sock = sock;
     while(1)
     {
         //等待客户端数据和检查是否在线
@@ -36,7 +41,14 @@ static int scp_recieve_respond( scp_pack_t* pack )
     return 0;
 }
 
-void uart_recieve_decoder_task( void* param )
+static int tcp_send( uint8_t* data , uint16_t len )
+{
+    if( _s_sock != -1 )
+        return send(_s_sock , data , len , 0 );
+    return -1;
+}
+
+static void uart_recieve_decoder_task( void* param )
 {
     uint8_t recieve_buf[512];
     uint8_t decoder_buf[512];
@@ -58,9 +70,18 @@ static void uart_send_scp_pack( scp_pack_t* pack )
     xSemaphoreGive( uart_tx_lock );
 }
 
+static void tcp_send_scp_pack( scp_pack_t* pack )
+{
+    if( _s_sock != -1 )
+    {
+        xSemaphoreTake( tcp_tx_lock , -1 );
+        scp_trans_send( pack , tcp_send );
+        xSemaphoreGive( tcp_tx_lock );
+    }
+}
+
 extern "C" void app_main(void)
 {   
-    TcpServer tcpServer;
     SysLed::sysLed.setFre( 1 , 200 );
     SysLed::sysLed.start();
 
@@ -68,23 +89,14 @@ extern "C" void app_main(void)
     bsp_uart_init();
 
     uart_tx_lock = xSemaphoreCreateBinary();
+    tcp_tx_lock = xSemaphoreCreateBinary();
 
-    // if( !bsp_wifi_connect( "MOSS(4316)" , "4316yyds" ) )
-    // {
-        // tcpServer.start( 144 , tcpApp , 4096 );
-    // }
-
-    // while(1)
-    // {
-    //     vTaskDelay(1);
-    // }
-
-    uint8_t temp[64];
-
-    while(1)
+    while( bsp_wifi_connect( "MOSS(4316)" , "4316yyds" ) != 0 )
     {
-        bsp_uart_send( (uint8_t*)"HelloWorld!" , strlen("HelloWorld!") );
-        ESP_LOGI( TAG , "recier func return  code : %d " , bsp_uart_recieve( temp , 60 , 10 ) );
-        vTaskDelay( 1000 / portTICK_PERIOD_MS );
+        ESP_LOGI( TAG , "retry to connect wifi." );
+        vTaskDelay( 500 / portTICK_PERIOD_MS );
     }
+
+    ESP_LOGI( TAG , "wifi connected , wait tcp client connect" );
+    tcpServer.start( 144 , tcpApp , 4096 );
 }
